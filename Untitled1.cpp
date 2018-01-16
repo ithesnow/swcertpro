f51f7ca15d8aab25384dc8bd7ef01b8b8bf1f1ff
vincent <ithesnow@gmail.com>
2018-01-16 06:54:05 +0000

    Frist commit on String

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+
+int i = 1;
+
+
 int main()
 {
 	std::cout << "Hello from AWS Cloud9!" << std::endl;
+	
+	for (int i = 0 ; i < 100 ; ++i){
+	   std::cout << i << std::endl;
+	}
 }
+
